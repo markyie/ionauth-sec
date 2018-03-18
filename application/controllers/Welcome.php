90b1d3d676ca0c8e87ad057d39857553ecf8ed7b
Sumri Mat Yajid <sumri7@gmail.com>
2018-03-19 00:17:50 +0800

    bugs correction in welcome-index

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,21 +3,33 @@
 use SimpleExcel\SimpleExcel;
 
 class Welcome extends CI_Controller {
-  public function test_composer()
-  {
-    $excel = new \SimpleExcel\SimpleExcel('xml'); // instantiate new object (will automatically construct the parser & writer type as XML)
 
-    $excel->writer->setData(
-      array
-      (
-        array('ID', 'Name', 'Kode' ),
-        array('1', 'Kab. Bogor', '1' ),
-        array('2', 'Kab. Cianjur', '1' ),
-        array('3', 'Kab. Sukabumi', '1' ),
-        array('4', 'Kab. Tasikmalaya', '2' )
-      )
-    ); // add some data to the writer
-    $excel->writer->saveFile('example'); // save the file with specified name (example.xml)
-    // and specified target (default to browser)
-  }
+    public function index()
+    {
+        $_SESSION['user'] = 'markyie';    
+        $this->load->view('welcome_message');
+    }
+
+    public function test_composer()
+    {
+      $excel = new \SimpleExcel\SimpleExcel('xml'); // instantiate new object (will automatically construct the parser & writer type as XML)
+
+      $excel->writer->setData(
+        array
+        (
+          array('ID', 'Name', 'Kode' ),
+          array('1', 'Kab. Bogor', '1' ),
+          array('2', 'Kab. Cianjur', '1' ),
+          array('3', 'Kab. Sukabumi', '1' ),
+          array('4', 'Kab. Tasikmalaya', '2' )
+        )
+      ); // add some data to the writer
+      $excel->writer->saveFile('example'); // save the file with specified name (example.xml)
+      // and specified target (default to browser)
+    }
+    
+    public function testing()
+    {
+        echo $_SESSION['user'];
+    }
 }
