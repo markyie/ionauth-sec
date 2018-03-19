f8d931d2512c857cbda9a956165a0cf814b91376
Sumri Mat Yajid <sumri7@gmail.com>
2018-03-19 16:28:03 +0800

    checkpoing - using template
    ref url : http://avenir.ro/codeigniter-tutorials/creating-using-page-templates-codeigniter/

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,17 +2,24 @@
 
 use SimpleExcel\SimpleExcel;
 
-class Welcome extends CI_Controller{
-
-    public function index()
-    {
-        $_SESSION['user'] = 'markyie';      
-
-        $this->load->view('welcome_message');
+class Welcome extends My_Controller{
+    
+    function __construct() {
+        parent::__construct();
     }
 
-    public function test_composer()
-    {
+    public function index(){
+        $_SESSION['user'] = 'markyie';      
+        $this->load->view('welcome_message');
+    }
+    
+    public function test_template(){
+        //$this->data['pagetitle'] = 'test'; ...you can at any time change the variables declared in the MY_Controller...
+        $this->render('homepage_view');
+        //$this->render(NULL, 'json'); ....if we want to render a json string. Also, if a request is made using ajax, we can simply do $this->render()
+    }
+
+    public function test_composer() {
       $excel = new \SimpleExcel\SimpleExcel('xml'); // instantiate new object (will automatically construct the parser & writer type as XML)
 
       $excel->writer->setData(
@@ -29,8 +36,7 @@ class Welcome extends CI_Controller{
       // and specified target (default to browser)
     }
     
-    public function testing()
-    {
+    public function testing() {
         echo $_SESSION['user'];
     }
 }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,11 +3,28 @@
 class MY_Controller extends CI_Controller
 {
     protected $data = array();
-    function __construct()
-    {
+    
+    function __construct(){
         parent::__construct();
-        $this->data['pagetitle'] = 'My CodeIgniter Apps';
+        $this->data['pagetitle'] = 'My CodeIgniter Apps';   
     }
+    
+    protected function render($the_view = NULL, $template = 'master')
+    {
+        if($template == 'json' || $this->input->is_ajax_request())
+        {
+            header('Content-Type: application/json');
+            echo json_encode($this->data);
+        }elseif(is_null($template)){
+            $this->load->view($this->data);
+        }
+        else
+        {
+          $this->data['the_view_content'] = (is_null($the_view)) ? '' : $this->load->view($the_view,$this->data, TRUE);
+          $this->load->view('templates/'.$template.'_view', $this->data);
+        }
+    }
+
 }
 
 class Admin_Controller extends MY_Controller
